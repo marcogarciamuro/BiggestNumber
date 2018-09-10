96129852013e8f8318e257373211b6f6c5b5bab2
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-09-10 16:54:36 -0700

    Determines biggest of 2 numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -3,22 +3,29 @@
 
 int main()
 {
-  int number;
+  int number1;
 
   std::cout<<"Please enter a whole number:\n";
-  std::cin>>number;
+  std::cin>>number1;
   
   int number2;
   std::cout<<"Please enter another whole number:\n";
   std::cin>>number2;
 
-  //determine if first or second is bigger and store that in a third variable
-
-
+  
+  int biggest;
   std::cout<<"Of those two numbers, the biggest is: ";
-  //print out the variable representing the biggest number
-
-  std::cout<<std::endl<<"Thank you for playing.\n";
+  if(number1>number2)
+  
+  {
+   std::cout<<number1;
+ 
+  }
+   else
+  {
+    std::cout<<number2;       
+  } 
+    std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
 }
