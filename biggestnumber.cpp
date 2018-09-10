3de26265be7669ccb544df2865213cf790af75d1
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-09-10 16:30:22 -0700

    Asks for number twice

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -3,13 +3,14 @@
 
 int main()
 {
-  //declare variables here
+  int number;
 
   std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
-
+  std::cin>>number;
+  
+  int number2;
   std::cout<<"Please enter another whole number:\n";
-  //get second input using std::cin>> again with a second variable
+  std::cin>>number2;
 
   //determine if first or second is bigger and store that in a third variable
 
